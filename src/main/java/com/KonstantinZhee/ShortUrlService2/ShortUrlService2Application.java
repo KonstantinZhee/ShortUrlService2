5c48df1f6f3b1467537f73264dbf5a7c2eb59abd
”Konstantin_Zhe” <konstantin.zheltukhin@devim.team>
2022-06-14 18:27:12 +0300

    add SpringBoot

---
+++ src/main/java/com/KonstantinZhee/ShortUrlService2/ShortUrlService2Application.java
@@ -0,0 +1,13 @@
+package com.KonstantinZhee.ShortUrlService2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortUrlService2Application.class, args);
+	}
+
+}
